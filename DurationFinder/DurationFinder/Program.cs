ff0567d8666101f6a6f2804505878815430142e8
danteCLRK <fryanslaucerers@gmail.com>
2017-06-07 23:30:03 -0400

    trying to get validations to work

--- DurationFinder/DurationFinder/Program.cs
+++ DurationFinder/DurationFinder/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DurationFinder
@@ -10,6 +11,26 @@ namespace DurationFinder
     {
         static void Main(string[] args)
         {
+            var reg = new Regex(@"\d{1,2}\/\d{1,2}\/\d{1,4}\s{0,}\,\s{0,}\d{1,2}\/\d{1,2}\/\d{1,4}");
+
+            Console.WriteLine("Please enter two dates in dd/mm/yyyy format seperated by a comma");
+            string datesIn = Console.ReadLine();
+
+            string tempDate = " ";
+            bool regMatch = reg.IsMatch(datesIn);
+
+            while (reg.IsMatch(datesIn) != true);
+            {
+                Console.WriteLine("Sorry, please make sure to enter your dates in dd/mm/yyyy format seperated by a comma.");
+                tempDate.Replace(" " , Console.ReadLine());
+                datesIn.Replace(datesIn, tempDate);
+            }
+
+            String[] dates = datesIn.Split(',');
+
+            foreach (var date in dates)
+                Console.WriteLine(date.Trim());
+            Console.ReadLine();
         }
     }
 }

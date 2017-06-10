8d89524a2c44ba5bcc05ef7f6cbaf338de013a3e
danteCLRK <fryanslaucerers@gmail.com>
2017-06-10 15:29:30 -0400

    Added comments to code

--- DurationFinder/DurationFinder/Program.cs
+++ DurationFinder/DurationFinder/Program.cs
@@ -8,29 +8,36 @@ namespace DurationFinder
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //gets dates from user
             Console.WriteLine("Please enter two dates in mm/dd/yyyy format seperated by a comma");
             string datesIn = Console.ReadLine();
 
+            //string array formats contains the accepted date formats
             string[] formats = {"M/d/yyyy h:mm:ss tt", "M/d/yyyy h:mm tt",
                          "MM/dd/yyyy hh:mm:ss", "M/d/yyyy h:mm:ss",
                          "M/d/yyyy hh:mm tt", "M/d/yyyy hh tt",
                          "M/d/yyyy h:mm", "M/d/yyyy h:mm",
                          "MM/dd/yyyy hh:mm", "M/dd/yyyy hh:mm", "MM/dd/yyyy", "M/d/yyyy","M/dd/yyyy", "MM/d/yyyy"};
 
+            //creates an empty list of strings which are used in the foreach loop below
             var parsedDates = new List<DateTime>();
 
+            //splits user input into an array of dates
             String[] dates = datesIn.Split(',');
 
+            //while loop makes sure that there are only two dates provided by the user
             while(dates.Length != 2)
             {   
                 Console.WriteLine("Please enter only two dates, in the mm/dd/yyyy format seperated by a comma");
                 dates = Console.ReadLine().Split(',');
             }
                 
-
+            // Sets a counter to count how many times the foreach loop is executed
             int count = 0;
           
+            //foreach loop loops through the dates entered and ensures that they are in the correct format. 
+            //if the dates are not in the correct format, the user is prompted to enter the dates again
             foreach (string date in dates)
             {
                 DateTime parsedDate;
@@ -50,13 +57,16 @@ namespace DurationFinder
                 count++;
             }
 
-
+            //checks that the dates are in the correct order in parsedDates
             if (parsedDates[0] > parsedDates[1])
                 parsedDates.Reverse();
 
+
+            //assigns dates to their own variables 
             var a = parsedDates[0];
             var b = parsedDates[1];
 
+            //math is done to determine time elapsed between the two provided dates
             var totalMonths = (b.Year - a.Year) * 12 + b.Month - a.Month;
             totalMonths += b.Day > a.Day ? -1 : 0;
             var years = totalMonths / 12;
